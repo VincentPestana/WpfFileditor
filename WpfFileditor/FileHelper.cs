cae2d0fd36b70e8133d0d84fd6b6f4fd9e91787e
Vincent Pestana <crashoveride001@gmail.com>
2020-01-13 19:49:22 +0200

    Added helper file with method that returns encodin on passed in file

---
+++ WpfFileditor/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+
+namespace WpfFileditor
+{
+    static class FileHelper
+	{
+        public static Encoding GetEncoding(string filename)
+        {
+            // This is a direct quote from MSDN:  
+            // The CurrentEncoding value can be different after the first
+            // call to any Read method of StreamReader, since encoding
+            // autodetection is not done until the first call to a Read method.
+
+            using (var reader = new StreamReader(filename, Encoding.Default, true))
+            {
+                if (reader.Peek() >= 0) // you need this!
+                    reader.Read();
+
+                return reader.CurrentEncoding;
+            }
+        }
+    }
+}
